ed15a03bed619b365557df13a16df357582183d9
Rufus <krisp1@protonmail.com>
2020-11-28 14:47:22 +1100

    Add initial blog listing 2

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,7 +4,7 @@ import BlogPost from './BlogPost';
 export default function BlogList() {
     const data = useStaticQuery(graphql`
       {
-          allMarkdownRemark(sort: { fields: frontmatter__date, order: DESC }) 
+          allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) 
 {
               edges {
                 node {
